77bfa25e78045401e3262825a1ad02f1bd12d8d3
rakzool <rahulmob.3607@gmail.com>
2020-05-29 01:43:46 +0530

    header&footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,17 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
     
   ],
   imports: [
@@ -31,7 +36,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
